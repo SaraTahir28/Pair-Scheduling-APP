d5b7da06d864d0dc84e54792ad78103df79f3472
Sara Tahir <saratahir34@gmail.com>
2026-03-11 09:40:57 +0000

    Added a simple default page for when we hit the route.

--- Backend/core/routes.py
+++ Backend/core/routes.py
@@ -1,6 +1,8 @@
 from django.urls import path #Django’s function for defining URL routes
 from .views import mock_booking
+from .base_views import index
 #mapping a URL path to our view
 urlpatterns = [
+    path("", index, name="index"),
     path("book/", mock_booking, name="mock_booking"),
 ]
