e4a4d48d4449e39dfbea90ba4dbdad6b5af03dab
Sara Tahir <saratahir34@gmail.com>
2026-03-13 12:27:33 +0000

    Adding a basic Html and Js file for demo tomorrow to show working prototype.

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,13 +1,27 @@
-async function createMeeting() {
+
+document.getElementById("booking_form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const jsonObject = {};
+
+    formData.forEach((value, key) => {
+        if (key === "start_time" || key === "end_time") {
+            // Convert "2026-03-10T14:00" → "2026-03-10T14:00:00Z"
+            value = value + ":00Z";
+        }
+        jsonObject[key] = value;
+    });
+
+    const json = JSON.stringify(jsonObject);
+    createMeeting(json);
+});
+
+async function createMeeting(json) {
   const response = await fetch("http://localhost:8000/api/create-meeting/", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      trainee_email: "trainee@example.com",
-      volunteer_email: "volunteer@example.com",
-      start_time: "2025-03-12T10:00:00",
-      end_time: "2025-03-12T11:00:00"
-    })
+    body: json
   });
 
   const data = await response.json();
