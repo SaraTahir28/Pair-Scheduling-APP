80efbee81bdfbb4f825fb43e282d6b0ba12b5ab6
Sara Tahir <saratahir34@gmail.com>
2026-03-18 21:41:20 +0000

    Deleting this extra file which was set up for the mock booking urls

--- Backend/core/routes.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path #Django’s function for defining URL routes
-from .views import mock_booking
-from .base_views import index
-#mapping a URL path to our view
-urlpatterns = [
-    path("", index, name="index"),
-    path("book/", mock_booking, name="mock_booking"),
-]
