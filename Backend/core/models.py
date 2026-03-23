db5bfac92b0c521027d99c1fe897e96ca470642e
Emiliano Uruena <emilianouz@yahoo.es>
2026-03-23 22:56:37 +0000

    Add custom user model for initial DATABASE and update settings to use it (#31)

--- Backend/core/models.py
+++ Backend/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    google_id = models.CharField(max_length=255, unique=True)
+    full_name = models.CharField(max_length=255, blank=True)
+
+    ROLE_CHOICES = [
+        ("volunteer", "Volunteer"),
+        ("trainee", "Trainee"),
+        ("admin", "Admin"),
+    ]
+
+    STATUS_CHOICES = [
+        ("active", "Active"),
+        ("disable", "Disable"),
+        ("banned", "Banned"),
+    ]
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="trainee")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="active")
+
+    def __str__(self):
+        return self.email or self.username
+    
+
