fc134edf3b3ad25312a3e8e45a5bbfade9ff7e53
Sara Tahir <saratahir34@gmail.com>
2026-03-25 18:39:50 +0000

    Removed the unused login import

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import ProtectedApp from "./ProtectedApp.jsx";
-import Login from "./components/pages/Login";
+
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
