7f06ffdd0a20804fb12f67ef550862839b16b774
Sara Tahir <saratahir34@gmail.com>
2026-03-30 10:43:28 +0100

    removed the unneccessary comments

--- Backend/core/policies/min_booking_window.py
+++ Backend/core/policies/min_booking_window.py
@@ -4,8 +4,7 @@ from django.utils import timezone
 class MinimumBookingWindowPolicy:
     MINIMUM_BOOKING_WINDOW = timedelta(hours=24) #to represent a duration of time 
 
-#using cls itself rather than object instance as the rule (24 hours) applies globally
-    
+
     @classmethod 
     def is_outside_window(cls, slot_start_time):
         earliest_allowed = timezone.now() + cls.MINIMUM_BOOKING_WINDOW
