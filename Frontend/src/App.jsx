08cbc8584d1083339ec10726bec9db61f0c90ce3
katarzynakaz <142831031+katarzynakaz@users.noreply.github.com>
2026-04-01 11:05:29 +0100

    Add a 404 page to improve routing feedback (#54)
    * 404 page ui polish
    
    * files from another pr removed
    
    * fix button change to a and errorElement removed
    
    * accidental deletion of navbar restored and unused import deleted
    
    ---------
    
    Co-authored-by: Duncan Parkinson <duncan.parkinson@mac.com>

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LandingPage from "./components/pages/LandingPage";
 import TraineeBookingFlow from "./components/pages/TraineeBookingFlow";
 import VolunteerDash from "./components/pages/VolunteerDash";
+import NotFound from "./components/pages/NotFound";
 import Navbar from "./components/elements/NavBar";
 
 const router = createBrowserRouter([
@@ -12,7 +13,6 @@ const router = createBrowserRouter([
 	{
 		path: "/trainee-booking",
 		children: [
-			// index true is root
 			{ index: true, Component: TraineeBookingFlow },
 			{ path: ":selectedDate", Component: TraineeBookingFlow },
 			{ path: ":selectedDate/:selectedTime", Component: TraineeBookingFlow },
@@ -21,23 +21,26 @@ const router = createBrowserRouter([
 	{
 		path: "/volunteer-dash",
 		children: [
-			// index true is root
 			{ index: true, Component: VolunteerDash },
 
 			{ path: "bookings/:id/edit", Component: VolunteerDash },
 		],
 	},
+	{
+		path: "*",
+		Component: NotFound,
+	},
 ]);
 
 function App() {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <div className="flex-1">
-        <RouterProvider router={router} />
-      </div>
-    </div>
-  );
+	return (
+		<div className="min-h-screen flex flex-col">
+			<Navbar />
+			<div className="flex-1">
+				<RouterProvider router={router} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
