f32fb25f1d969813373b23a2851ed4ee8aac6514
Emiliano Uruena <emilianouz@yahoo.es>
2026-04-07 22:47:07 +0100

    API - SlotRule creation endpoint  (#69)
    * Add SlotRule serializer with validation
    
    * Add SlotRule create API POST /api/slot-rules/
    
    * url slot rule creation endpoint route
    
    * Adding the missing permission class ensures that only logged-in users can create SlotRule.
    
    * Removing volunteer role constraint.
    
    * Ensure that slot_rules can only be created by the authenticated user for their own profile.
    
    * Updated field volunteer as read_only
    
    * Refactor validation method: repeat_until is after start_time

---
+++ Backend/core/serializers/slot_rule_serializer.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from core.models import SlotRule, User
+
+
+class SlotRuleSerializer(serializers.ModelSerializer):
+   
+    class Meta:
+        model = SlotRule
+        fields = [
+            "id",
+            "volunteer",
+            "start_time",
+            "repeat_until",
+            "group",
+        ]
+        read_only_fields = [
+            "id",
+            "volunteer"
+        ]
+
+    def validate_repeat_until_is_after_start_time(self, attrs):
+        start_time = attrs.get("start_time")
+        repeat_until = attrs.get("repeat_until")
+        if repeat_until is not None and start_time is not None:
+            if repeat_until < start_time.date():
+                raise serializers.ValidationError(
+                    {"repeat_until": "repeat_until cannot be earlier than start_time date."}
+                )
+        return attrs
+
+    def validate(self, attrs):
+        attrs = self.validate_repeat_until_is_after_start_time(attrs)
+        return attrs
+
+    def create(self, validated_data):
+
+        slot_rule = SlotRule(**validated_data)
+        slot_rule.full_clean()
+        slot_rule.save()
+        return slot_rule
--- Backend/core/urls.py
+++ Backend/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import create_meeting_view
 from .views import MeView
-from .views import UserListCreateView, UserDetailView
+from .views import UserListCreateView, UserDetailView, SlotRuleCreateView
 
 # API routes for calendar-related actions
 urlpatterns = [
@@ -11,4 +11,5 @@ urlpatterns = [
     path("users/", UserListCreateView.as_view(), name="user-list-create"),
     path("users/<int:pk>/", UserDetailView.as_view(), name="user-detail"),
     path("me/", MeView.as_view(), name="me"),
+    path("slot-rules/", SlotRuleCreateView.as_view(), name="slot-rule-create"),
 ]
