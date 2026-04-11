e30b3553c2faa795b855b5bd53aefd58d1c71083
katarzynakaz <l453r3@gmail.com>
2026-04-11 16:27:12 +0100

    trainee test

--- Frontend/cypress/e2e/Integration-Trainee.cy.jsx
+++ Frontend/cypress/e2e/Integration-Trainee.cy.jsx
@@ -1,39 +1,37 @@
 describe("Trainee booking flow", () => {
+	beforeEach(() => {
+		cy.intercept("GET", "**/auth/user/", {
+			statusCode: 200,
+			body: { id: 1, name: "Duncan Parkinson", email: "duncan@example.com" },
+		}).as("getUser");
+
+		cy.intercept("GET", "**/auth/csrf/", {
+			statusCode: 200,
+			body: { detail: "CSRF cookie set" },
+		}).as("getCsrf");
+
+		cy.intercept("POST", "**/api/create-meeting/", {
+			statusCode: 201,
+			body: { message: "Meeting created successfully.", event_id: "abc123" },
+		}).as("postBooking");
+	});
+
 	it("allows a trainee to book a session from start to finish", () => {
-		beforeEach(() => {
-			cy.intercept("GET", "**/auth/user/", {
-				statusCode: 200,
-				body: { id: 1, name: "Duncan Parkinson", email: "duncan@example.com" },
-			}).as("getUser");
+		cy.visit("/trainee-booking");
+		cy.wait("@getUser");
 
-			cy.intercept("GET", "**/auth/csrf/", {
-				statusCode: 200,
-				body: { detail: "CSRF cookie set" },
-			}).as("getCsrf");
+		cy.get(".cal-day-available").first().click();
+		cy.get(".timeslot-group-div button").first().click();
 
-			cy.intercept("POST", "**/api/create-meeting/", {
-				statusCode: 201,
-				body: { message: "Meeting created successfully.", event_id: "abc123" },
-			}).as("postBooking");
-		});
+		cy.get("input.form-input").first().clear().type("Kaska Test");
+		cy.get("button").contains("Book meeting").click();
 
-		it("allows a trainee to book a session from start to finish", () => {
-			cy.visit("/trainee-booking");
-			cy.wait("@getUser");
+		cy.wait("@postBooking");
 
-			cy.get(".cal-day-available").first().click();
-			cy.get(".timeslot-group-div button").first().click();
+		cy.url().should("include", "/confirmation");
 
-			cy.get("input.form-input").first().clear().type("Kaska Test");
-			cy.get("button").contains("Book meeting").click();
+		cy.get(".conf-page-div").should("be.visible");
 
-			cy.wait("@postBooking");
-
-			cy.url().should("include", "/confirmation");
-
-			cy.get(".conf-page-div").should("be.visible");
-
-			cy.get(".session-details-col").should("not.exist");
-		});
+		cy.get(".session-details-col").should("not.exist");
 	});
 });
