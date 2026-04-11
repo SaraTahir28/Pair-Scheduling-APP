6e83df25198dbe7bee59a58e68d06d310b0a8004
katarzynakaz <142831031+katarzynakaz@users.noreply.github.com>
2026-04-11 14:23:32 +0100

    Cypress tests for the trainee booking flow (#103)
    * trainee booking tests
    
    * fix - connect TraineeBookingFlow to AuthContext and fix form validation
    
    * cal test
    
    * calendar test fixed
    
    * redundant components removed
    
    * Cleanup: remove ignored files from git tracking
    
    * redundant comments removed from TraineeBookingFlow
    
    * failing test fixed

---
+++ Frontend/cypress/tests/SessionDetails.cy.jsx
@@ -0,0 +1,35 @@
+import SessionDetails from "../../src/components/groups/SessionDetails";
+import duncanImage from "../../src/assets/duncan.png";
+
+describe("SessionDetails Component tests", () => {
+	const testedVolunteer = {
+		name: "Duncan",
+		img: duncanImage,
+	};
+
+	it("shows trainee view by default", () => {
+		cy.mount(
+			<SessionDetails
+				activeVolunteerProps={testedVolunteer}
+				volunteerView={false}
+			/>
+		);
+
+		cy.contains("Book 1:1 session").should("be.visible");
+		cy.contains("Your session is with").should("be.visible");
+		cy.contains("Duncan").should("be.visible");
+	});
+
+	it("shows volunteer view when volunteerView is true", () => {
+		cy.mount(
+			<SessionDetails
+				activeVolunteerProps={testedVolunteer}
+				volunteerView={true}
+			/>
+		);
+
+		cy.contains("Welcome back").should("be.visible");
+		cy.contains("You are logged in as").should("be.visible");
+		cy.contains("Duncan").should("be.visible");
+	});
+});
