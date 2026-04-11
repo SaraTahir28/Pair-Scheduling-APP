fe97a904adfa459e24c9ba56aa5769056a9e21f2
katarzynakaz <l453r3@gmail.com>
2026-04-11 16:21:09 +0100

    mock response added

--- Frontend/cypress/e2e/Integration-Trainee.cy.jsx
+++ Frontend/cypress/e2e/Integration-Trainee.cy.jsx
@@ -1,29 +1,39 @@
 describe("Trainee booking flow", () => {
 	it("allows a trainee to book a session from start to finish", () => {
-		cy.intercept("GET", "**/auth/user/", {
-			body: { id: 1, name: "Duncan Parkinson", email: "duncan@test.com" },
-		}).as("getUser");
+		beforeEach(() => {
+			cy.intercept("GET", "**/auth/user/", {
+				statusCode: 200,
+				body: { id: 1, name: "Duncan Parkinson", email: "duncan@example.com" },
+			}).as("getUser");
 
-		cy.intercept("POST", "**/api/create-meeting/", {
-			statusCode: 201,
-			body: { message: "Meeting created successfully.", event_id: "abc123" },
-		}).as("postBooking");
+			cy.intercept("GET", "**/auth/csrf/", {
+				statusCode: 200,
+				body: { detail: "CSRF cookie set" },
+			}).as("getCsrf");
 
-		cy.visit("/trainee-booking");
-		cy.wait("@getUser");
+			cy.intercept("POST", "**/api/create-meeting/", {
+				statusCode: 201,
+				body: { message: "Meeting created successfully.", event_id: "abc123" },
+			}).as("postBooking");
+		});
 
-		cy.get(".cal-day-available").first().click();
-		cy.get(".timeslot-group-div button").first().click();
+		it("allows a trainee to book a session from start to finish", () => {
+			cy.visit("/trainee-booking");
+			cy.wait("@getUser");
 
-		cy.get("input.form-input").first().clear().type("Kaska Test");
-		cy.get("button").contains("Book meeting").click();
+			cy.get(".cal-day-available").first().click();
+			cy.get(".timeslot-group-div button").first().click();
 
-		cy.wait("@postBooking");
+			cy.get("input.form-input").first().clear().type("Kaska Test");
+			cy.get("button").contains("Book meeting").click();
 
-		cy.url().should("include", "/confirmation");
+			cy.wait("@postBooking");
 
-		cy.get(".conf-page-div").should("be.visible");
+			cy.url().should("include", "/confirmation");
 
-		cy.get(".session-details-col").should("not.exist");
+			cy.get(".conf-page-div").should("be.visible");
+
+			cy.get(".session-details-col").should("not.exist");
+		});
 	});
 });
