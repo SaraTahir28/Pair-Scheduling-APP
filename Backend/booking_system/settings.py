705098e3c20cd9a05611a46a2032ac58f4375f8b
Sara Tahir <saratahir34@gmail.com>
2026-04-12 10:16:59 +0500

    Moved the secret key to .env file, removed debug statements and extra comment.

--- Backend/booking_system/settings.py
+++ Backend/booking_system/settings.py
@@ -34,13 +34,13 @@ GOOGLE_OAUTH2_CLIENT_SECRET = env("GOOGLE_CLIENT_SECRET")
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(#1c#^ypty_2-2!3g^uu9p@z0=#2+7nv8qz#swf@yg&okj0ld1"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DJANGO_ENV != "production"
 
 ALLOWED_HOSTS = [
-    "pairscheduler-backend.hosting.codeyourfuture.io",  # production backend
+    "pairscheduler-backend.hosting.codeyourfuture.io",
     "localhost",
     "127.0.0.1",
 ]
@@ -256,6 +256,3 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5174",
     "https://pairscheduler-frontend.hosting.codeyourfuture.io",
 ]
-
-print("DJANGO_ENV:", DJANGO_ENV)
-print("FRONTEND_URL loaded:", FRONTEND_URL)
