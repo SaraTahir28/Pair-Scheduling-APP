f5f80e782aaee8809d189384da91c84b0b96c0b7
Emilianouz <135679131+Emilianouz@users.noreply.github.com>
2026-04-13 10:43:43 +0100

    Add end_time field to AvailableSlotSerializer / JSON

--- Backend/core/serializers/available_slot_serializer.py
+++ Backend/core/serializers/available_slot_serializer.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 class AvailableSlotSerializer(serializers.Serializer):
     slot_rule_id = serializers.IntegerField()
     start_time = serializers.DateTimeField()
+    end_time = serializers.DateTimeField()
     volunteer_id = serializers.IntegerField()
     name = serializers.CharField()
     img = serializers.CharField()
