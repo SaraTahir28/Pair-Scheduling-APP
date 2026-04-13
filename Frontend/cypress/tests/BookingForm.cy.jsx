89a9c96d8c2be8ee26dcd4eb90720f4a58d2e4d5
katarzynakaz <l453r3@gmail.com>
2026-04-13 12:32:12 +0100

    booking form component test updated to pass - greeting, validation for alert and agenda

--- Frontend/cypress/tests/BookingForm.cy.jsx
+++ Frontend/cypress/tests/BookingForm.cy.jsx
@@ -1,55 +1,42 @@
 import BookingForm from "../../src/components/groups/BookingForm";
 
 describe("BookingForm Component tests", () => {
-	it("shows the tested name and email and verifies email is readOnly", () => {
-		const testedTrainee = { name: "Test Trainee", email: "trainee@test.com" };
+	it("shows the name in label greting", () => {
+		const testedTrainee = { name: "Kaska Test", email: "kaska@test.com" };
 
 		cy.mount(<BookingForm trainee={testedTrainee} whenFormSubmit={() => {}} />);
 
-		cy.get("input.form-input").first().should("have.value", "Test Trainee");
-
-		cy.get("input.form-input-disabled")
-			.should("have.value", "trainee@test.com")
-			.and("have.attr", "readonly");
+		cy.get("label.form-label").should(
+			"contain",
+			"Okay Kaska, what would you like to discuss?"
+		);
 	});
 
-	it("triggers alert when the name is too short", () => {
-		const testedTrainee = { name: "T", email: "trainee@test.com" };
-
-		cy.mount(<BookingForm trainee={testedTrainee} whenFormSubmit={() => {}} />);
-
-		cy.on("window:alert", (text) => {
-			expect(text).to.contains("Name should be between 2 and 50 chars.");
-		});
-
-		cy.get("button").contains("Book meeting").click();
-	});
-
-	it("cleans name by removing numbers and symbols on submit", () => {
+	it("sends alert when agenda is too short", () => {
 		const testedSubmit = cy.stub().as("testedSubmit");
-		const testedTrainee = { name: "", email: "trainee@test.com" };
+		const testedTrainee = { name: "Kaska", email: "kaska@test.com" };
 
 		cy.mount(
 			<BookingForm trainee={testedTrainee} whenFormSubmit={testedSubmit} />
 		);
-
-		cy.get("input.form-input").first().type("Kaska123?");
-		cy.get("button").contains("Book meeting").click();
-
-		cy.get("@testedSubmit").should("have.been.calledWith", {
-			traineeName: "Kaska",
-			traineeEmail: "trainee@test.com",
+		cy.on("window:alert", (text) => {
+			expect(text).to.contains("Agenda should be between 10 and 500 chars.");
 		});
+
+		cy.get("textarea.form-input").type("Too short");
+		cy.get("button").contains("Book meeting").click();
 	});
 
-	it("allows typing in the name input", () => {
-		const testedTrainee = { name: "", email: "trainee@test.com" };
+	it("allows typing in the agenda field", () => {
+		const testedTrainee = { name: "Kaska", email: "kaska@test.com" };
 
 		cy.mount(<BookingForm trainee={testedTrainee} whenFormSubmit={() => {}} />);
 
-		cy.get("input.form-input")
-			.first()
-			.type("New Name")
-			.should("have.value", "New Name");
+		cy.get("textarea.form-input")
+			.type("I want to learn about passing props between components in React")
+			.should(
+				"have.value",
+				"I want to learn about passing props between components in React"
+			);
 	});
 });
