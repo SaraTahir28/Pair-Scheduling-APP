585956793937c1c5120dda31accd5c225d73c1a4
Emilianouz <135679131+Emilianouz@users.noreply.github.com>
2026-04-13 11:27:54 +0100

    Use username as fallback for volunteer name in available slots

--- Backend/core/services/available_slots.py
+++ Backend/core/services/available_slots.py
@@ -25,7 +25,7 @@ def build_available_slots(rules, beginning_of_booking_window):
                     group=rule.group,
                     start_time=start_time,
                     end_time=start_time + timedelta(hours=1),
-                    name=rule.volunteer.get_full_name(),
+                    name=rule.volunteer.get_full_name() or rule.volunteer.get_username(),
                     img="/public/placeholder.png",
                 )
             )
