ed7fe93f60c010061bfe2567656eda3f43a4589c
Sara Tahir <saratahir34@gmail.com>
2026-04-16 00:02:39 +0500

    Removing redundant import

--- Backend/auth_app/views/loginlogout.py
+++ Backend/auth_app/views/loginlogout.py
@@ -1,6 +1,5 @@
 from django.middleware.csrf import get_token
 from django.contrib.auth import logout
-from django.contrib.auth import logout
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
