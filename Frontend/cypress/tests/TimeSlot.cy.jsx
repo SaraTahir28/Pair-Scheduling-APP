95769ca8e03ef53084d2c208931dfd36089c25f2
Emilianouz <135679131+Emilianouz@users.noreply.github.com>
2026-04-16 19:11:47 +0100

    Refactor TimeSlotGroup tests to use structured time objects

--- Frontend/cypress/tests/TimeSlot.cy.jsx
+++ Frontend/cypress/tests/TimeSlot.cy.jsx
@@ -1,47 +1,73 @@
 import TimeSlotGroup from "../../src/components/groups/TimeSlotGroup";
 
 describe("TimeSlotGroup component tests", () => {
-	it("shows the prompt when no date is selected", () => {
-		cy.mount(
-			<TimeSlotGroup
-				selectedDateProps={null}
-				setSelectedTimeProps={() => {}}
-				availableTimes={[]}
-			/>
-		);
+  it("shows the prompt when no date is selected", () => {
+    cy.mount(
+      <TimeSlotGroup
+        selectedDateProps={null}
+        setSelectedTimeProps={() => {}}
+        availableTimes={[]}
+      />
+    );
 
-		cy.contains("Select a date to see available times.").should("be.visible");
-	});
+    cy.contains("Select a date to see available times.").should("be.visible");
+  });
 
-	it("shows the tested date and available times when a date is picked", () => {
-		const testedDate = new Date(2026, 4, 30);
-		const testedTimes = ["09:00", "14:30"];
+  it("shows the tested date and available times when a date is picked", () => {
+    const testedDate = new Date(2026, 4, 30);
+    const testedTimes = [
+      {
+        time: "09:00",
+        volunteerId: 1,
+        slotRuleId: 1,
+        name: "Duncan Parkinson",
+      },
+      {
+        time: "14:30",
+        volunteerId: 2,
+        slotRuleId: 2,
+        name: "Jane Smith",
+      },
+    ];
 
-		cy.mount(
-			<TimeSlotGroup
-				selectedDateProps={testedDate}
-				setSelectedTimeProps={() => {}}
-				availableTimes={testedTimes}
-			/>
-		);
+    cy.mount(
+      <TimeSlotGroup
+        selectedDateProps={testedDate}
+        setSelectedTimeProps={() => {}}
+        availableTimes={testedTimes}
+      />
+    );
 
-		cy.contains(testedDate.toDateString()).should("be.visible");
-		cy.contains("09:00").should("be.visible");
-		cy.contains("14:30").should("be.visible");
-	});
+    cy.contains(testedDate.toDateString()).should("be.visible");
+    cy.contains("09:00").should("be.visible");
+    cy.contains("14:30").should("be.visible");
+  });
 
-	it("does not show times that are not in the tested list", () => {
-		const testedDate = new Date(2026, 4, 30);
-		const testedTimes = ["09:00", "14:30"];
+  it("does not show times that are not in the tested list", () => {
+    const testedDate = new Date(2026, 4, 30);
+    const testedTimes = [
+      {
+        time: "09:00",
+        volunteerId: 1,
+        slotRuleId: 1,
+        name: "Duncan Parkinson",
+      },
+      {
+        time: "14:30",
+        volunteerId: 2,
+        slotRuleId: 2,
+        name: "Jane Smith",
+      },
+    ];
 
-		cy.mount(
-			<TimeSlotGroup
-				selectedDateProps={testedDate}
-				setSelectedTimeProps={() => {}}
-				availableTimes={testedTimes}
-			/>
-		);
+    cy.mount(
+      <TimeSlotGroup
+        selectedDateProps={testedDate}
+        setSelectedTimeProps={() => {}}
+        availableTimes={testedTimes}
+      />
+    );
 
-		cy.contains("16:00").should("not.exist");
-	});
+    cy.contains("16:00").should("not.exist");
+  });
 });
