d36439eddfaa4aba3942f9331596e321acbc6cda
Sara Tahir <saratahir34@gmail.com>
2026-04-17 22:33:42 +0500

    Feature/navbabackbutton (#172)
    Co-authored-by: Emilianouz <135679131+Emilianouz@users.noreply.github.com>

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,54 +1,59 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import LandingPage from "./components/pages/LandingPage";
 import TraineeBookingFlow from "./components/pages/TraineeBookingFlow";
 import VolunteerDash from "./components/pages/VolunteerDash";
 import NotFound from "./components/pages/NotFound";
 import Navbar from "./components/elements/NavBar";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: LandingPage,
-  },
-  {
-    path: "/trainee-booking",
-    children: [
-      { index: true, Component: TraineeBookingFlow },
-      { path: ":selectedDate", Component: TraineeBookingFlow },
-      { path: ":selectedDate/:selectedTime", Component: TraineeBookingFlow },
-      {
-        path: ":selectedDate/:selectedTime/:status",
-        Component: TraineeBookingFlow,
-      },
-      {
-        path: ":selectedDate/:selectedTime/:status/:volunteerId/:slotRuleId",
-        Component: TraineeBookingFlow,
-      },
-    ],
-  },
-  {
-    path: "/volunteer-dash",
-    children: [
-      { index: true, Component: VolunteerDash },
-
-      { path: "bookings/:id/edit", Component: VolunteerDash },
-    ],
-  },
-  {
-    path: "*",
-    Component: NotFound,
-  },
-]);
-
-function App() {
+function Layout() {
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
       <div className="flex-1">
-        <RouterProvider router={router} />
+        <Outlet />
       </div>
     </div>
   );
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />, // Wrap everything in Layout
+    children: [
+      { path: "/", Component: LandingPage },
+      {
+        path: "/trainee-booking",
+        children: [
+          { index: true, Component: TraineeBookingFlow },
+          { path: ":selectedDate", Component: TraineeBookingFlow },
+          {
+            path: ":selectedDate/:selectedTime",
+            Component: TraineeBookingFlow,
+          },
+          {
+            path: ":selectedDate/:selectedTime/:status",
+            Component: TraineeBookingFlow,
+          },
+          {
+            path: ":selectedDate/:selectedTime/:status/:volunteerId/:slotRuleId",
+            Component: TraineeBookingFlow,
+          },
+        ],
+      },
+      {
+        path: "/volunteer-dash",
+        children: [
+          { index: true, Component: VolunteerDash },
+          { path: "bookings/:id/edit", Component: VolunteerDash },
+        ],
+      },
+      { path: "*", Component: NotFound },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
