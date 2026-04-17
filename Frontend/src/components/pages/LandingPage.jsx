ff18aea14bf1d640a967d0d84b764ef8a30aa991
Sara Tahir <saratahir34@gmail.com>
2026-04-17 10:26:28 +0500

    changing heading to h2 to avoid jumping headings levels

--- Frontend/src/components/pages/LandingPage.jsx
+++ Frontend/src/components/pages/LandingPage.jsx
@@ -13,14 +13,14 @@ const LandingPage = () => {
           <div className="role-title-icon">
             <Laptop size={32} strokeWidth={2} />
           </div>
-          <h3>I am a Trainee</h3>
+          <h2>I am a Trainee</h2>
           <p>Book a 1:1 pair programming session with a CYF volunteer.</p>
         </Link>
         <Link to="/volunteer-dash" className="role-tile role-tile-volunteer">
           <div className="role-title-icon">
             <Users size={32} strokeWidth={2} />
           </div>
-          <h3>I am a Volunteer</h3>
+          <h2>I am a Volunteer</h2>
           <p>Manage your availability and view upcoming sessions.</p>
         </Link>
       </div>
