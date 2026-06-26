58956f532e4c73d94a0f9c5d2ad0b4cba9d40362
Sara Tahir <saratahir34@gmail.com>
2026-06-26 22:08:46 +0100

    Updated the view to use the new serializer

--- Backend/core/views.py
+++ Backend/core/views.py
@@ -21,6 +21,7 @@ from .serializers.available_slot_serializer import AvailableSlotSerializer
 from .serializers.booking_serializer import BookingSerializer
 from .serializers.slot_rule_exception_serializer import SlotRuleExceptionSerializer
 from .serializers.slot_rule_serializer import SlotRuleSerializer
+from .serializers.volunteer_public_serializer import VolunteerPublicSerializer
 
 # Local Serializers
 from .user_serializers import UserSerializer
@@ -231,5 +232,5 @@ class VolunteerByDateTimeView(APIView):
             role="volunteer",
         )
 
-        serializer = UserSerializer(volunteers, many=True)
+        serializer = VolunteerPublicSerializer(volunteers, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
