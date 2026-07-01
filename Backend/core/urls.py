72c926b9655c98a399645d87726c33b202786d93
katarzynakaz <l453r3@gmail.com>
2026-07-01 16:01:03 +0100

    delete UserDetailView and its route

--- Backend/core/urls.py
+++ Backend/core/urls.py
@@ -6,7 +6,6 @@ from .views import (
     CurrentProfileView,
     SlotRuleDeleteView,
     SlotRuleListCreateView,
-    UserDetailView,
     UserListCreateView,
 )
 
@@ -15,7 +14,6 @@ urlpatterns = [
     path("create-meeting/", CreateMeetingView.as_view(), name="create_meeting"),
     # API routes for Users in database Endpoints
     path("users/", UserListCreateView.as_view(), name="user-list-create"),
-    path("users/<int:pk>/", UserDetailView.as_view(), name="user-detail"),
     path("available-slots/", AvailableSlotsView.as_view(), name="available-slots"),
     path("profile/", CurrentProfileView.as_view(), name="current-profile"),
     path(
